a5c8f7ced97e7ea13ebaa836562727fa64578421
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2016-05-23 21:21:14 +0200

    Add correct report via google api to report view

--- src/main/java/de/eichstaedt/ui/controller/AusgabenKategorieReportController.java
+++ src/main/java/de/eichstaedt/ui/controller/AusgabenKategorieReportController.java
@@ -1,7 +1,10 @@
 package de.eichstaedt.ui.controller;
 
+import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
@@ -19,10 +22,34 @@ public class AusgabenKategorieReportController {
 	@Autowired
 	private AusgabenKategorieReport reportRepository;
 	
+	private int counter;
+	
+	private Logger logger = LoggerFactory.getLogger(AusgabenKategorieReportController.class);
+	
 	@RequestMapping("kategorieReport")
 	public String getAusgabenKategorieReport(ModelMap model) {
 		
 		model.addAttribute("reports", (List<AusgabenKategorieBericht>)reportRepository.findAll());
+		counter = 1;
+		
+		logger.info("Generate the report with counter {} and all reports {} ",counter,((List<AusgabenKategorieBericht>)reportRepository.findAll()).size());
+		
+		Object [][] reportArray = new Object[((List<AusgabenKategorieBericht>)reportRepository.findAll()).size()+1][2];
+		reportArray[0][0] = "Ausgabe";
+		reportArray[0][1] = "Kategorie";
+		
+		((List<AusgabenKategorieBericht>)reportRepository.findAll()).stream().forEach((bericht)->{
+			logger.info("Put report array {}",reportArray.toString());
+			reportArray[counter][0] = bericht.getKategorie().getName();
+			reportArray[counter][1] = Math.round(bericht.getBetrag());
+			counter++;
+		});
+		
+		
+		logger.info("Report Array {}", Arrays.toString(reportArray));
+		
+		
+		model.addAttribute("reportArray", reportArray);
 		
 		return "ausgabenKategorieReport";
 	}
