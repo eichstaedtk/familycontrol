2140dc99a4ad4f69a7668b971691d82cb1bc6a39
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2016-05-31 21:50:54 +0200

    Integration of Spring Security Login and Logout Handling

--- src/main/java/de/eichstaedt/FamilyappApplication.java
+++ src/main/java/de/eichstaedt/FamilyappApplication.java
@@ -6,8 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 import de.eichstaedt.domain.services.ReportObserver;
@@ -16,7 +21,9 @@ import de.eichstaedt.ui.controller.AusgabenController;
 
 @SpringBootApplication
 @EnableJpaRepositories("de.eichstaedt")
-public class FamilyappApplication {
+@Configuration
+@EnableWebSecurity
+public class FamilyappApplication extends WebSecurityConfigurerAdapter{
 
   @Autowired
   private AusgabenController controller;
@@ -31,6 +38,25 @@ public class FamilyappApplication {
     SpringApplication.run(FamilyappApplication.class, args);
 
     logger.info("FamilyControl Application started");
+  }
+  
+  protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll().and()
+            .logout().logoutUrl("/logout");
+		
+	}
+  
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+      auth
+          .inMemoryAuthentication()
+              .withUser("konrad").password("Start123").roles("USER");
   }
 
   @Bean
--- src/main/java/de/eichstaedt/ui/controller/LoginController.java
+++ src/main/java/de/eichstaedt/ui/controller/LoginController.java
@@ -19,6 +19,14 @@ public class LoginController {
 
     return "login";
   }
+  
+  @RequestMapping(value = "/logout", method = RequestMethod.GET)
+  public String logout(ModelMap model) {
+
+    model.addAttribute("auth", Authentication.build("", ""));
+
+    return "logout";
+  }
 
   @RequestMapping(value = "/login", method = RequestMethod.POST)
   public String login() {
