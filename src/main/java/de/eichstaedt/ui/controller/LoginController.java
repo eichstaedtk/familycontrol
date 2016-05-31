b995035f1817881eddeab1a16281d1e4c337bb4d
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2016-05-31 22:02:55 +0200

    Merge

--- src/main/java/de/eichstaedt/ui/controller/LoginController.java
+++ src/main/java/de/eichstaedt/ui/controller/LoginController.java
@@ -1,7 +1,13 @@
 package de.eichstaedt.ui.controller;
 
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,10 +18,17 @@ public class LoginController {
 
   private Authentication authentification;
 
+  private Logger logger = LoggerFactory.getLogger(LoginController.class);
+
   @RequestMapping(value = "/login", method = RequestMethod.GET)
   public String login(ModelMap model) {
 
-    model.addAttribute("auth", Authentication.build("", ""));
+    authentification = Authentication.build("", "");
+
+    model.addAttribute("auth", authentification);
+
+    logger.info("Getting authentification object with login {} and password {}",
+        authentification.getLoginName(), authentification.getPassword());
 
     return "login";
   }
@@ -29,7 +42,10 @@ public class LoginController {
   }
 
   @RequestMapping(value = "/login", method = RequestMethod.POST)
-  public String login() {
+  public String login(@Valid Authentication auth, BindingResult bindingResult, Model model) {
+
+    logger.info("Login POST with auth login {} and password {} ", auth.getLoginName(),
+        auth.getPassword());
 
     return "redirect:ausgaben/kategorieReport";
   }
